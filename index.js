1ea1060051b00c2246609311acd9901503867e9c
gabriel gostoso <128156868+gabrielfernandes777@users.noreply.github.com>
2024-08-12 09:04:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const precosLivre = [25, 15, 30,50, 45, 20];
 
 let atual = 0;
 let maisbarato = 0;
+
+for(let atual = 0; atual < precosLivre.length; atual ++){
+    if(precosLivre [atual] <precosLivre [maisbarato]){
+        maisbarato = atual
+    }
+}
+console.log( `O livreo mais barato custa ${precosLivre [maisbarato]}`)
